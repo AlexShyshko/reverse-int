2ac616fd70e7d05613f20e24ec277e372735c964
AlexShyshko <shish1988@mail.ru>
2021-06-21 13:51:39 +0300

    feat: write function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    let originalNumber = n;
+    if (originalNumber % 10 === 0) {
+      originalNumber = originalNumber / 10;
+    }
+    if (originalNumber < 0) {
+      originalNumber = Math.abs(originalNumber);
+    }
+    originalNumber = String(originalNumber);
+    originalNumber = originalNumber.split('').reverse().join('');
+    return originalNumber = Number(originalNumber);
+  }
